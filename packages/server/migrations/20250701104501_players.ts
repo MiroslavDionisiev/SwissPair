a600c56f2112d28402d1698d18c230891979f281
Phantomvv1 <krisi_andreev@abv.bg>
2025-07-01 15:06:09 +0300

    #14 - Added onDelete and a function that updates the updated_at field automatically

--- packages/server/migrations/20250701104501_players.ts
+++ packages/server/migrations/20250701104501_players.ts
@@ -3,12 +3,30 @@ import type { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable("players", (table) => {
     table.increments("id").primary();
-    table.integer("tournament_id").notNullable().references("tournaments");
+    table.integer("tournament_id").notNullable().references("id").inTable("tournaments").onDelete("CASCADE");
     table.text("player_name").notNullable();
     table.timestamp("created_at").defaultTo(knex.fn.now()).notNullable();
     table.timestamp("updated_at").defaultTo(knex.fn.now()).notNullable();
   });
+
+  await knex.raw(`
+    CREATE OR REPLACE FUNCTION update_updated_at_column()
+    RETURNS TRIGGER AS $$
+    BEGIN
+      NEW.updated_at = current_timestamp;
+      RETURN NEW;
+    END;
+    $$ language 'plpgsql';
+  `);
+
+  await knex.raw(`
+    CREATE TRIGGER update_players_updated_at
+    BEFORE UPDATE ON players
+    FOR EACH ROW
+    EXECUTE function update_updated_at_column();
+  `);
 }
+
 
 export async function down(knex: Knex): Promise<void> {
   await knex.schema.dropTableIfExists("players");
