86f30da7f88947df536eaa08ce8452ca5e14e667
Phantomvv1 <krisi_andreev@abv.bg>
2025-07-02 12:10:59 +0300

    #18 Made the player model. Also readied myself for the relation with the tournament

---
+++ packages/server/src/models/player_model.ts
@@ -0,0 +1,41 @@
+import { BaseModel } from "./base"
+//import { Tournament } from "./tournament_model"
+
+export class Player extends BaseModel {
+  id!: number;
+  tournament_id!: string;
+  name!: string;
+  created_at!: string;
+  updated_at!: string;
+
+  static get tableName() {
+    return "players";
+  }
+
+  static get jsonSchema() {
+    return {
+      type: "object",
+      required: ["tournament_id", "name"],
+      properties: {
+        id: { type: "integer" },
+        tournament_id: { type: "string", format: "uuid" },
+        name: { type: "string" },
+        created_at: { type: "string", format: "date-time" },
+        updated_at: { type: "string", format: "date-time" },
+      },
+    };
+  }
+
+  //static get relationMappings() {
+  //  return {
+  //    tournament: {
+  //      relation: BaseModel.BelongsToOneRelation,
+  //      modelClass: Tournament,
+  //      join: {
+  //        from: "players.tournament_id",
+  //        to: "tournaments.id",
+  //      },
+  //    },
+  //  };
+  //}
+}
