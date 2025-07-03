e39ce2ffe532f21673c763a4e756992116741f4a
adrian <adrian.e.yanakiev@gmail.com>
2025-07-03 11:22:51 +0300

    Primary key made an integer, removed foreign keys

--- packages/server/migrations/20250702132921_rounds.ts
+++ packages/server/migrations/20250702132921_rounds.ts
@@ -3,11 +3,11 @@ import type { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
     return await knex.schema.createTable('rounds', (table) => {
-        table.uuid('id').primary();
+        table.increments('id').primary();
         table.uuid('tournamen_id').notNullable().references('id').inTable('tournaments');
         table.integer('round_number').notNullable();
-        table.uuid('player_white_id').notNullable().references('id').inTable('players');
-        table.uuid('player_black_id').notNullable().references('id').inTable('players');
+        table.integer('player_white_id');
+        table.integer('player_black_id');
         table.enum('round_result', ['whiteWon', 'draw', 'blackWon']).nullable().defaultTo(null);
         table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable();
         table.timestamp('updated_at').defaultTo(knex.fn.now()).notNullable();
