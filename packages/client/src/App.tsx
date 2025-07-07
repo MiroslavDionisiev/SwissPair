5fb5eed14e12137ca96d6339b2444023c444f0b0
Stoyan Ivanov <101361817+soletki@users.noreply.github.com>
2025-07-07 13:52:22 +0300

    Table label

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,20 +1,10 @@
 import './app.module.css';
 import './index.css';
-import { TablesComponent } from './components/TablesComponent';
-
-const tournaments = [
-  { name: "Plovdiv Chess Open", players: 20, rounds: "1/5", time: "00:00:13" },
-  { name: "The Checkmate Classic", players: 190, rounds: "2/8", time: "00:12:00" },
-  { name: "Crown Open", players: 1024, rounds: "1/10", time: "00:23:05" },
-  { name: "King’s Arena", players: 9, rounds: "2/4", time: "01:23:40" },
-  { name: "The Queen’s Challenge", players: 124, rounds: "5/7", time: "02:33:34" },
-];
 
 export function App() {
   return (
     <div>
       test
-     <TablesComponent tournaments={tournaments} />
     </div>
   );
 }
--- packages/client/src/components/TablesComponent.tsx
+++ packages/client/src/components/TablesComponent.tsx
@@ -7,16 +7,34 @@ interface Tournament {
   time: string;
 }
 
-export function TablesComponent({ tournaments }: { tournaments: Tournament[] }) {
+const wrapperStyle = {
+  clipPath: 'polygon(16px 0, calc(100% - 16px) 0, 100% 16px, 100% 100%, 0 100%, 0 16px)',
+  backgroundColor: 'black',
+  display: 'inline-block',
+  padding: '1px',
+}
+
+const labelStyle = {
+  clipPath: 'polygon(16px 0, calc(100% - 16px) 0, 100% 16px, 100% 100%, 0 100%, 0 16px)'
+}
+
+export function TablesComponent({ tournaments, tableName }: { tournaments: Tournament[], tableName: string }) {
   return (
-    <div className="overflow-x-auto w-[960px] h-[392px] mx-auto">
+    <div className="overflow-x-auto px-[10%] mx-auto">
+      <div style={wrapperStyle}>
+        <div
+          className="bg-yellow-dark text-xl py-[0.5rem] px-[2rem] font-bold text-center"
+          style={labelStyle}
+        >{tableName}</div>
+      </div>
+
       <table className="min-w-full w-full h-full border border-black">
         <thead>
           <tr className="bg-black">
-            <th className="text-yellow-dark py-3 px-4 text-left font-bold text-lg border-b border-black">NAME</th>
-            <th className="text-yellow-dark py-3 px-4 text-center font-bold text-lg border-b border-black">PLAYERS</th>
-            <th className="text-yellow-dark py-3 px-4 text-center font-bold text-lg border-b border-black">ROUNDS</th>
-            <th className="text-yellow-dark py-3 px-4 text-center font-bold text-lg border-b border-black">TIME</th>
+            <th className="text-yellow-dark py-3 px-4 text-left font text-lg border-b border-black">NAME</th>
+            <th className="text-yellow-dark py-3 px-4 text-center font text-lg border-b border-black">PLAYERS</th>
+            <th className="text-yellow-dark py-3 px-4 text-center font text-lg border-b border-black">ROUNDS</th>
+            <th className="text-yellow-dark py-3 px-4 text-center font text-lg border-b border-black">TIME</th>
           </tr>
         </thead>
         <tbody>
