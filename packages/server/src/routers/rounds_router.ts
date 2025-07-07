4c829cbec8b12f38164cc5b9e310c28bf3e47674
Phantomvv1 <krisi_andreev@abv.bg>
2025-07-07 13:12:51 +0300

    #29 Made a function that gets all of the rounds in a tournament and returns every player's score up until that point

--- packages/server/src/routers/rounds_router.ts
+++ packages/server/src/routers/rounds_router.ts
@@ -1,6 +1,7 @@
 import express from "express"
+import { RoundModel, RoundStatus } from "../models/round"
 
-export class Player {
+export class SwissPlayer {
   id!: String | Number// unique identifier
   score!: Number// current score
   pairedUpDown?: Boolean// if the player has been paired up/down prior (optional)
@@ -20,6 +21,38 @@ export class Player {
 
 export const RoundRouter = express.Router()
 
+function getPlayerScores(rounds: RoundModel[]): SwissPlayer[] {
+  const scores: Map<number | string, SwissPlayer> = new Map();
+
+  for (const round of rounds) {
+    const whiteId = round.playerWhiteId;
+    const blackId = round.playerBlackId;
+    const result = round.roundResult;
+
+    if (!scores.has(whiteId)) {
+      scores.set(whiteId, new SwissPlayer(whiteId, 0));
+    }
+
+    if (!scores.has(blackId)) {
+      scores.set(blackId, new SwissPlayer(blackId, 0));
+    }
+
+    const whitePlayer = scores.get(whiteId)!;
+    const blackPlayer = scores.get(blackId)!;
+
+    if (result === RoundStatus.whiteWon) {
+      whitePlayer.score = Number(whitePlayer.score) + 1;
+    } else if (result === RoundStatus.blackWon) {
+      blackPlayer.score = Number(blackPlayer.score) + 1;
+    } else if (result === RoundStatus.draw) {
+      whitePlayer.score = Number(whitePlayer.score) + 0.5;
+      blackPlayer.score = Number(blackPlayer.score) + 0.5;
+    }
+  }
+
+  return Array.from(scores.values());
+}
+
 async function getAllRounds(req: express.Request, res: express.Response) {
 
 }
@@ -32,6 +65,7 @@ async function updateRound(req: express.Request, res: express.Response) {
 
 }
 
+//Returns array of objects of type Player
 async function createRounds(req: express.Request, res: express.Response) {
 
 }
