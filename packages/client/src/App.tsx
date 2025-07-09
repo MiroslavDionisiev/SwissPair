3eb77d39d83692712c9d6a0c957db56843418a8a
Stoyan Ivanov <101361817+soletki@users.noreply.github.com>
2025-07-09 14:50:21 +0300

    Removed stuff from App.tsx

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,10 +1,11 @@
 import './app.module.css';
 import './index.css';
-import TournamentPage from './pages/TournamentPage';
 
 export function App() {
   return (
-    <TournamentPage></TournamentPage>
+    <div>
+      test
+    </div>
   );
 }
   
