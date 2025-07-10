0423a0351da515740bc3f8744a10ccd0d289cfb1
RKDimitrov <asrr3fl3x@gmail.com>
2025-07-10 10:54:56 +0300

    HomePageRouter

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,10 +1,15 @@
 import './app.module.css';
 import './index.css';
 import { HomePage } from './pages/HomePage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 export function App() {
   return (
-    <HomePage/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
   
