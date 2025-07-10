93fde79b7797ed15085d1d72e2519eb128cf9a7f
Stoyan Ivanov <101361817+soletki@users.noreply.github.com>
2025-07-10 11:36:36 +0300

    Changed Routing

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router';
+import { BrowserRouter, Route, Routes } from 'react-router';
 import './app.module.css';
 import './index.css';
 import TournamentPage from './pages/TournamentPage';
@@ -7,11 +7,13 @@ import TournamentResults from './pages/TournamentResults';
 
 export function App() {
   return (
-    <Routes>
-      <Route path="tournament/:tournamentId" element={<TournamentPage />}>
-        <Route index element={<NewTournament />} />
-        <Route path="results" element={<TournamentResults />} />
-      </Route>
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="tournament/:tournamentId" element={<TournamentPage />}>
+          <Route index element={<NewTournament />} />
+          <Route path="results" element={<TournamentResults />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,10 +1,5 @@
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
-import { BrowserRouter } from 'react-router';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion -- This element will always be present
-createRoot(document.getElementById('root')!).render(
-    <BrowserRouter>
-        <App/>
-    </BrowserRouter>
-);
+createRoot(document.getElementById('root')!).render(<App />);
